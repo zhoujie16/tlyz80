466af0370beb42e3747e24cb3dd74b5398caebe0
zhoujie <1406187962@qq.com>
2018-06-10 22:28:01 +0800

    选项参数

--- js/http.js
+++ js/http.js
@@ -1,40 +1,39 @@
 (function() {
-	return;
 	var url = 'http://101.201.196.132/school_webapi';
-	$.ajax({
-		type: "post",
-		url: url + '/grade/list',
-		async: true,
-		success: function(data) {
-			console.log('gradeList', data);
-			var gradeList = data.dataList;
+	//查学届
+	axios.post(url + '/grade/list')
+		.then(function(response) {
+			var gradeList = response.data.dataList;
 
-		},
-		error: function(err) {
-			console.log(err)
-		}
-	});
-	$.ajax({
-		type: "post",
-		url: url + '/class/list',
-		async: true,
-		traditional:true,
-		contentType:'application/json;charset=UTF-8',
-		data: {
-			campusId: null,
-			className: null,
-			gradeYear: null,
-			headTeacherName: null,
+			console.log('学姐gradeList', gradeList);
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+
+	//查校区 
+	axios.get(url + '/campus')
+		.then(function(response) {
+			var campusList = response.data.dataList;
+
+			console.log('查校区campus', campusList);
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+
+	//查班级
+	axios.post(url + '/class/list', {
 			pageNum: 1,
-			pageSize: 20
-		},
-		success: function(data) {
-			console.log('classList', data);
-			var classList = data.dataList;
-
-		},
-		error: function(err) {
-			console.log(err)
-		}
-	});
+			pageSize: 200,
+			campusId: "6",
+			gradeYear: "1991"
+		})
+		.then(function(response) {
+			var classList = response.data.dataList;
+			console.log('查班级 classList', classList);
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
 }());
